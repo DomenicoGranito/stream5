76c3770776d3a443262cae544d2a38e367f4b5e2
gargankit20 <gargankit936@gmail.com>
2017-02-28 13:57:57 +0530

    latest

--- Streamini/Controllers/MyLibViewController.swift
+++ Streamini/Controllers/MyLibViewController.swift
@@ -15,87 +15,27 @@ class RecentlyPlayedCell:UITableViewCell
 
 class MyLibViewController: UIViewController
 {
-    @IBOutlet var itemsTbl:UITableView?
-    
     let menuItemTitlesArray=["Playlists", "Live Streams", "Videos", "Series", "Channels", "Upcoming Events"]
     let menuItemIconsArray=["playlist", "youtube", "internet", "Series", "channels", "time"]
     
     var recentlyPlayed:[NSManagedObject]?
-    var user: User?
-    var profileDelegate: ProfileDelegate?
-    var selectedImage: UIImage?
-
-   // var dataSource: UserStatisticsDataSource?
-    var dataSource: UserSelecting?
-    var type: ProfileStatisticsType = .Following
-   
-
-   // override func viewDidLoad()
-   // {
-       // super.viewDidLoad()
-       // self.configureView()
-        
-       // let activator=UIActivityIndicatorView(activityIndicatorStyle:.White)
-       // activator.startAnimating()
-        
-       // self.navigationItem.rightBarButtonItem=UIBarButtonItem(customView:activator)
-     //   UserConnector().get(nil, success:successGetUser, failure:successFailure)
-   // }
-
-    override func viewDidLoad()
-    {
-        recentlyPlayed=SongManager.getRecentlyPlayed()
-        
-        self.tabBarController!.navigationItem.hidesBackButton=true
-        navigationController?.navigationBarHidden=false
-        self.navigationController!.setNavigationBarHidden(false, animated:true)
-        UIApplication.sharedApplication().setStatusBarHidden(false, withAnimation:.Fade)
-    }
-
-   // @IBOutlet var MyProfile : UIButton!
-   // override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
-     //   if (segue.identifier == "MyProfile") {
-         //   let playlistName = sender as! String
-       //     let playlistVC = (segue.destinationViewController as? UserViewController)!
-           // playlistVC.playlistName = playlistName
-            //UserProfileViewController
-        //}
-    //}
-    
-   // @IBAction func MyProfilePressed(sender:AnyObject)
-    //{
-     //   self.performSegueWithIdentifier("MyProfile", sender:self)
-   // }
     
     override func viewWillAppear(animated:Bool)
     {
-        self.tabBarController!.navigationItem.hidesBackButton = true
-        navigationController?.navigationBarHidden=false
-        self.navigationController!.setNavigationBarHidden(false, animated: true)
-        UIApplication.sharedApplication().setStatusBarHidden(false, withAnimation:.Fade)
+        recentlyPlayed=SongManager.getRecentlyPlayed()
     }
     
-    @IBAction func myaccount(user:User)
+    @IBAction func myaccount()
     {
         let storyboard=UIStoryboard(name:"Main", bundle:nil)
         let vc=storyboard.instantiateViewControllerWithIdentifier("myProfileViewControllerId") as! myProfileViewController
         vc.user=UserContainer.shared.logged()
         navigationController?.pushViewController(vc, animated:true)
     }
-
-    func successGetUser(user:User)
-    {
-        self.user=user
-    }
-    
-    func successFailure(error:NSError)
-    {
-      //  handleError(error)
-    }
     
     func tableView(tableView:UITableView, heightForRowAtIndexPath indexPath:NSIndexPath)->CGFloat
     {
-        if(indexPath.row<6)
+        if(indexPath.row<7)
         {
             return 44
         }
@@ -107,7 +47,7 @@ class MyLibViewController: UIViewController
     
     func tableView(tableView:UITableView, numberOfRowsInSection section:Int)->Int
     {
-        return recentlyPlayed!.count+6
+        return recentlyPlayed!.count+7
     }
     
     func tableView(tableView:UITableView, cellForRowAtIndexPath indexPath:NSIndexPath)->UITableViewCell
@@ -121,15 +61,19 @@ class MyLibViewController: UIViewController
             
             return cell
         }
+        else if(indexPath.row==6)
+        {
+            let cell=tableView.dequeueReusableCellWithIdentifier("Cell")!
+            
+            return cell
+        }
         else
         {
             let cell=tableView.dequeueReusableCellWithIdentifier("RecentlyPlayedCell") as! RecentlyPlayedCell
             
-            cell.videoTitleLbl?.text=recentlyPlayed![indexPath.row-6].valueForKey("songName") as? String
+            cell.videoTitleLbl?.text=recentlyPlayed![indexPath.row-7].valueForKey("songName") as? String
             
             return cell
         }
-        
-        return UITableViewCell()
     }
 }
