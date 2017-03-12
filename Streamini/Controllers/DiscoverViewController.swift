176a46da7ba352327873dbed93a38f01b1269660
gargankit20 <gargankit936@gmail.com>
2017-03-12 16:23:10 +0530

    latest

--- Streamini/Controllers/DiscoverViewController.swift
+++ Streamini/Controllers/DiscoverViewController.swift
@@ -1,65 +1,3 @@
-class UILoadingView : UIView {
-    
-    init(frame rect: CGRect, text: NSString = "Loading...") {
-        super.init(frame: rect)
-        self.backgroundColor = UIColor(colorLiteralRed:18/255, green:19/255, blue:21/255, alpha:1)
-        self.label.text = text as String
-        self.label.textColor = self.spinner.color
-        
-        
-        
-        
-        self.spinner.startAnimating()
-        
-        self.addSubview(self.label)
-        self.addSubview(self.spinner)
-        
-        //self.autoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight;
-        
-        self.setNeedsLayout()
-        
-    }
-    
-    required init(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)!
-    }
-    
-    lazy var label : UILabel = {
-        var l = UILabel()
-        l.font = UIFont.systemFontOfSize(UIFont.systemFontSize())
-        return l
-    }()
-    var spinner: UIActivityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.Gray)
-    
-    override func layoutSubviews() {
-        var labelString:NSString = self.label.text!
-        var labelSize: CGSize = labelString.sizeWithAttributes([NSFontAttributeName: self.label.font])
-        var labelFrame: CGRect = CGRect()
-        labelFrame.size = labelSize
-        self.label.frame = labelFrame
-        
-        // center label and spinner
-        self.label.center = self.center
-        self.spinner.center = self.center
-        
-        // horizontally align
-        labelFrame = self.label.frame
-        var spinnerFrame: CGRect = self.spinner.frame
-        var totalWidth: CGFloat = spinnerFrame.size.width + 5 + labelSize.width
-        spinnerFrame.origin.x = self.bounds.origin.x + (self.bounds.size.width - totalWidth) / 2
-        labelFrame.origin.x = spinnerFrame.origin.x + spinnerFrame.size.width + 5
-        self.label.frame = labelFrame
-        self.spinner.frame = spinnerFrame
-    }
-    
-}
-
-class MenuCell: UITableViewCell
-{
-    @IBOutlet var menuItemTitleLbl:UILabel?
-    @IBOutlet var menuItemIconImageView:UIImageView?
-}
-
 //
 //  CategoriesViewController.swift
 //  Streamini
@@ -67,6 +5,12 @@ class MenuCell: UITableViewCell
 //  Created by Ankit Garg on 9/9/16.
 //  Copyright © 2016 UniProgy s.r.o. All rights reserved.
 //
+
+class MenuCell: UITableViewCell
+{
+    @IBOutlet var menuItemTitleLbl:UILabel?
+    @IBOutlet var menuItemIconImageView:UIImageView?
+}
 
 class DiscoverViewController:BaseTableViewController
 {
