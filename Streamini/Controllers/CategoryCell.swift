250f13e37e6477aa1896750bd77cad66b345b158
gargankit20 <gargankit936@gmail.com>
2017-03-15 12:33:48 +0530

    latest

--- Streamini/Controllers/CategoryCell.swift
+++ Streamini/Controllers/CategoryCell.swift
@@ -9,7 +9,5 @@
 class CategoryCell: UICollectionViewCell
 {
     @IBOutlet var videoTitleLbl:UILabel?
-    @IBOutlet var followersCountLbl:UILabel?
     @IBOutlet var videoThumbnailImageView:UIImageView?
-    @IBOutlet var iconcategory:UIImageView?
 }
