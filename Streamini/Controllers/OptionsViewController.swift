f3a4c8de62a7386cd0346e145b8255cc2b64f1f8
gargankit20 <gargankit936@gmail.com>
2017-04-06 12:13:52 +0530

    latest

--- Streamini/Controllers/OptionsViewController.swift
+++ Streamini/Controllers/OptionsViewController.swift
@@ -8,8 +8,31 @@
 
 class OptionsViewController: UIViewController
 {
+    @IBOutlet var backgroundImageView:UIImageView?
+    
+    let menuItemTitlesArray:NSMutableArray=["Custom", "Title", "Artist", "Recently Added"]
+    
     override func viewDidLoad()
     {
         
+    }
+    
+    @IBAction func closeButtonPressed()
+    {
+        dismissViewControllerAnimated(true, completion:nil)
+    }
+    
+    func tableView(tableView:UITableView, numberOfRowsInSection section:Int)->Int
+    {
+        return 4
+    }
+    
+    func tableView(tableView:UITableView, cellForRowAtIndexPath indexPath:NSIndexPath)->UITableViewCell
+    {
+        let cell=tableView.dequeueReusableCellWithIdentifier("MenuCell") as! MenuCell
+        
+        cell.menuItemTitleLbl?.text=menuItemTitlesArray[indexPath.row] as? String
+        
+        return cell
     }
 }
