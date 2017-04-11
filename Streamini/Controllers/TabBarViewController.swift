4140aec4c0ef6583432a0bf469f5bb6660f339e7
gargankit20 <gargankit936@gmail.com>
2017-04-11 16:23:24 +0530

    latest

--- Streamini/Controllers/TabBarViewController.swift
+++ Streamini/Controllers/TabBarViewController.swift
@@ -8,20 +8,26 @@
 
 import Photos
 
-class TabBarViewController: UITabBarController , UITabBarControllerDelegate
+class TabBarViewController: UITabBarController, UITabBarControllerDelegate
 {
     @IBOutlet var vtabBar:UITabBar!
     @IBOutlet var miniPlayerView:UIView!
-  
     @IBOutlet var containerView:UIView!
     @IBOutlet var playView:UIView!
-    @IBOutlet var miniPlayerButton : UIButton!
+    @IBOutlet var miniPlayerButton:UIButton!
     
-    func tapMiniPlayer()
+    var animator:ARNTransitionAnimator!
+    var modalVC:ModalViewController!
+    
+    override func viewDidLoad()
     {
-        presentViewController(modalVC, animated:true, completion:nil)
+        let storyboard=UIStoryboard(name:"Main", bundle:nil)
+        modalVC=storyboard.instantiateViewControllerWithIdentifier("ModalViewController") as? ModalViewController
+        
+        setupAnimator()
+        getPermissions()
     }
-
+    
     override func viewWillAppear(animated:Bool)
     {
         NSNotificationCenter.defaultCenter().addObserver(self, selector:#selector(goToChannels), name:"goToChannels", object:nil)
@@ -44,18 +50,19 @@ class TabBarViewController: UITabBarController , UITabBarControllerDelegate
         playView.hidden=true
     }
     
-    func showButton()
-    {
-        playView.hidden=false
-        //  view.bringSubviewToFront(playView)
-    }
-    
-    var animator:ARNTransitionAnimator!
-    var modalVC:ModalViewController!
-    
     @IBAction func tapMiniPlayerButton()
     {
         presentViewController(modalVC, animated:true, completion:nil)
+    }
+    
+    func tabBarController(tabBarController:UITabBarController, shouldSelectViewController viewController:UIViewController)->Bool
+    {
+        let tabViewControllers=tabBarController.viewControllers
+        let fromIndex=tabViewControllers?.indexOf(tabBarController.selectedViewController!)
+        
+        NSUserDefaults.standardUserDefaults().setInteger(fromIndex!, forKey:"previousTab")
+        
+        return true
     }
     
     func setupAnimator()
@@ -86,106 +93,21 @@ class TabBarViewController: UITabBarController , UITabBarControllerDelegate
         modalVC.transitioningDelegate=animator
     }
     
-    func tabBarController(tabBarController:UITabBarController, shouldSelectViewController viewController:UIViewController)->Bool
+    func getPermissions()
     {
-        let tabViewControllers=tabBarController.viewControllers
-        let fromIndex=tabViewControllers?.indexOf(tabBarController.selectedViewController!)
-        
-        NSUserDefaults.standardUserDefaults().setInteger(fromIndex!, forKey:"previousTab")
-        
-        return true
-    }
-    
-    @IBAction func recButtonPressed(sender:AnyObject)
-    {
-        self.performSegueWithIdentifier("RootToCreate", sender:self)
-    }
-    
-    func setupMainNavigationItems()
-    {
-        self.title=""
-        
-        let button=UIButton(frame:CGRectMake(0, 0, 125, 25))
-        button.setImage(UIImage(named:"global"), forState:.Normal)
-        button.setImageTintColor(UIColor(white:1, alpha:0.5), forState:.Normal)
-        button.setImageTintColor(UIColor(white:1, alpha:1), forState:.Highlighted)
-        let item=UIBarButtonItem(customView:button)
-        
-        self.navigationItem.rightBarButtonItem=item
-    }
-    
-    func searchTapped()
-    {
-        let storyboard=UIStoryboard(name:"Main", bundle:nil)
-        let controller=storyboard.instantiateViewControllerWithIdentifier("SearchScreen")
-        self.presentViewController(controller, animated:true, completion:nil)
-    }
-    
-    override func viewDidLoad()
-    {
-        self.delegate = self
-        self.tabBarController?.delegate = self
-        let storyboard=UIStoryboard(name:"Main", bundle:nil)
-        modalVC=storyboard.instantiateViewControllerWithIdentifier("ModalViewController") as? ModalViewController
-        
-        setupAnimator()
-        
-        setupMainNavigationItems()
-        
-        // Ask for use Camera
         if AVCaptureDevice.respondsToSelector(#selector(AVCaptureDevice.requestAccessForMediaType(_:completionHandler:)))
         {
-            AVCaptureDevice.requestAccessForMediaType(AVMediaTypeVideo, completionHandler:{(granted)->Void in })
+            AVCaptureDevice.requestAccessForMediaType(AVMediaTypeVideo, completionHandler:{(granted)->Void in})
         }
         
-        // Ask for use Microphone
         if(AVAudioSession.sharedInstance().respondsToSelector(#selector(AVAudioSession.requestRecordPermission(_:))))
         {
-            AVAudioSession.sharedInstance().requestRecordPermission({(granted:Bool)->Void in })
+            AVAudioSession.sharedInstance().requestRecordPermission({(granted:Bool)->Void in})
         }
         
-        // Ask for use Photo Gallery
         if NSClassFromString("PHPhotoLibrary") != nil
         {
-            if #available(iOS 8.0, *)
-            {
-                PHPhotoLibrary.requestAuthorization{(status)->Void in }
-            }
+            PHPhotoLibrary.requestAuthorization{(status)->Void in}
         }
-        
-       
-        //  self.view.addSubview(playView)
-    }
-
-    
-    func addCenterButton() {
-        let xPosition: [CGFloat] = [5.0, 5.0]
-        let yPosition: [CGFloat] = [5.0, 5.0]
-        //let xPosition = 10.0
-        //let yPosition = 10.0
-        //self.tabBarController?.tabBar.frame.origin.x
-        //View will slide 20px up
-       // let yPosition = playView.frame.origin.y +
-        
-      //  let height = playView.frame.size.height
-       // let width = playView.frame.size.width
-//xPosition, yPosition, height, width
-       // playView.frame = CGRectMake(300, 520,  50,  50)
-//self.view.frame = CGRectMake(300, 520,  50,  50)
-        
-            //CGRectMake(300, 520,  50,  50)
-       // playView.bounds.origin.y
-    //    let bounds = self.playView.bounds
-     
-        
-      // self.playView.bounds.origin.y = 300
-       // self.playView.bounds.origin.x = 300
-        self.playView.frame.origin.y = 20
-        self.playView.frame.origin.x = 0
-        self.playView.frame.size.width = 375
-        self.view.addSubview(playView)
-       // self.playView.bounds = CGRectMake( 600,  100, 60, 50)
-        
-      //  self.playView.bounds.origin.y = 100
     }
 }
