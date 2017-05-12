293949b4a41afb484809b71447bd2688c75ce975
gargankit20 <gargankit936@gmail.com>
2017-05-12 18:27:50 +0530

    latest

--- Streamini/Controllers/PasswordViewController.swift
+++ Streamini/Controllers/PasswordViewController.swift
@@ -6,56 +6,53 @@
 //  Copyright © 2016 UniProgy s.r.o. All rights reserved.
 //
 
-class PasswordViewController: BaseViewController {
-
-    @IBOutlet weak var currentPassword: UITextField!
-    @IBOutlet weak var newPassword: UITextField!
-    @IBOutlet weak var confirmPassword: UITextField!
+class PasswordViewController: BaseViewController
+{
+    @IBOutlet var currentPassword:UITextField!
+    @IBOutlet var newPassword:UITextField!
+    @IBOutlet var confirmPassword:UITextField!
     
     override func viewDidLoad()
     {
-        let doneBarButtonItem=UIBarButtonItem(barButtonSystemItem:.Done, target:self, action:#selector(doneButtonPressed))
-        self.navigationItem.rightBarButtonItem = doneBarButtonItem
-        
-        currentPassword.placeholder = NSLocalizedString("current_password", comment: "")
-        newPassword.placeholder = NSLocalizedString("new_password", comment: "")
-        confirmPassword.placeholder = NSLocalizedString("confirm_password", comment: "")
+        currentPassword.placeholder=NSLocalizedString("current_password", comment:"")
+        newPassword.placeholder=NSLocalizedString("new_password", comment:"")
+        confirmPassword.placeholder=NSLocalizedString("confirm_password", comment:"")
     }
     
     override func viewWillAppear(animated:Bool)
     {
-        currentPassword.text = ""
-        newPassword.text = ""
-        confirmPassword.text = ""
+        currentPassword.text=""
+        newPassword.text=""
+        confirmPassword.text=""
     }
     
-    func doneButtonPressed(sender: AnyObject) {
-        
-        if let _ = A0SimpleKeychain().stringForKey("password") {
-            if(A0SimpleKeychain().stringForKey("password") != currentPassword.text){
-                let alertView = UIAlertView.notAuthorizedAlert(NSLocalizedString("current_password_wrong", comment: ""))
+    @IBAction func doneButtonPressed()
+    {
+        if let _=A0SimpleKeychain().stringForKey("password")
+        {
+            if(A0SimpleKeychain().stringForKey("password") != currentPassword.text)
+            {
+                let alertView=UIAlertView.notAuthorizedAlert(NSLocalizedString("current_password_wrong", comment:""))
                 alertView.show()
                 return
             }
         }
         
-        if(newPassword.text != confirmPassword.text || newPassword.text == "")
+        if(newPassword.text != confirmPassword.text||newPassword.text=="")
         {
-            let alertView = UIAlertView.notAuthorizedAlert(NSLocalizedString("passwords_do_not_match", comment: ""))
+            let alertView=UIAlertView.notAuthorizedAlert(NSLocalizedString("passwords_do_not_match", comment:""))
             alertView.show()
             return
         }
         
-        let text=newPassword.text!
-        
-        UserConnector().password(text, success: passwordSuccess, failure: passwordFailure)
+        UserConnector().password(newPassword.text!, success:passwordSuccess, failure:passwordFailure)
     }
-
+    
     func passwordSuccess()
     {
-        let alertView = UIAlertView.notAuthorizedAlert(NSLocalizedString("password_changed", comment: ""))
+        let alertView=UIAlertView.notAuthorizedAlert(NSLocalizedString("password_changed", comment:""))
         alertView.show()
-        A0SimpleKeychain().setString(newPassword.text!, forKey: "password")
+        A0SimpleKeychain().setString(newPassword.text!, forKey:"password")
     }
     
     func passwordFailure(error:NSError)
